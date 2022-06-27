18e15d47f25607ab4f95048db6c8331483244a61
AxeAfk <gonghe@dodoex.io>
2022-06-27 12:50:14 +0800

    fix safe tx hash

--- src/logic/safe/store/actions/createTransaction.ts
+++ src/logic/safe/store/actions/createTransaction.ts
@@ -346,7 +346,20 @@ export const createTransaction = (
     }
 
     // SafeTxHash acts as the unique ID of a tx throughout the app
-    sender.safeTxHash = generateSafeTxHash(txProps.safeAddress, sender.safeVersion, sender.txArgs)
+    sender.safeTxHash = await sender.safeInstance.methods
+      .getTransactionHash(
+        sender.txArgs.to,
+        sender.txArgs.valueInWei,
+        sender.txArgs.data,
+        sender.txArgs.operation,
+        sender.txArgs.safeTxGas,
+        sender.txArgs.baseGas,
+        sender.txArgs.gasPrice,
+        sender.txArgs.gasToken,
+        sender.txArgs.refundReceiver || '',
+        sender.txArgs.nonce,
+      )
+      .call()
 
     // Start the creation
     sender.submitTx(confirmCallback, errorCallback)

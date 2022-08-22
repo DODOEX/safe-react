fb24f24f50260b3e1300e3f40d30b3f186b654d4
AxeAfk <gonghe@dodoex.io>
2022-08-22 10:11:22 +0800

    add tokenlist

--- src/logic/safe/api/fetchTokenCurrenciesBalances.ts
+++ src/logic/safe/api/fetchTokenCurrenciesBalances.ts
@@ -18,6 +18,41 @@ type FetchTokenCurrenciesBalancesProps = {
   trustedTokens?: boolean
 }
 
+const getBalanceCall = (address, tokenAddress) => {
+  const web3 = getWeb3ReadOnly()
+  let balanceCall
+  if (tokenAddress === '0x0000000000000000000000000000000000000000') {
+    balanceCall = web3.eth.getBalance(address)
+  } else {
+    const instance = new web3.eth.Contract(
+      [
+        {
+          inputs: [
+            {
+              internalType: 'address',
+              name: 'owner',
+              type: 'address',
+            },
+          ],
+          name: 'balanceOf',
+          outputs: [
+            {
+              internalType: 'uint256',
+              name: 'balance',
+              type: 'uint256',
+            },
+          ],
+          stateMutability: 'view',
+          type: 'function',
+        },
+      ],
+      tokenAddress,
+    )
+    balanceCall = instance.methods.balanceOf(address).call()
+  }
+  return balanceCall
+}
+
 export const fetchTokenCurrenciesBalances = async ({
   safeAddress,
   selectedCurrency,
@@ -25,65 +60,64 @@ export const fetchTokenCurrenciesBalances = async ({
   trustedTokens = false,
 }: FetchTokenCurrenciesBalancesProps): Promise<SafeBalanceResponse> => {
   const address = checksumAddress(safeAddress)
-  const web3 = getWeb3ReadOnly()
-  const erc20Instance = new web3.eth.Contract(
-    [
-      {
-        inputs: [
-          {
-            internalType: 'address',
-            name: 'owner',
-            type: 'address',
-          },
-        ],
-        name: 'balanceOf',
-        outputs: [
-          {
-            internalType: 'uint256',
-            name: 'balance',
-            type: 'uint256',
-          },
-        ],
-        stateMutability: 'view',
-        type: 'function',
-      },
-    ],
-    '0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d',
+  const tokenlist = [
+    {
+      type: TokenType.NATIVE_TOKEN,
+      address: '0x0000000000000000000000000000000000000000',
+      decimals: 18,
+      symbol: 'BNB',
+      name: 'BNB',
+      logoUri: 'https://safe-transaction-assets.gnosis-safe.io/chains/56/currency_logo.png',
+    },
+    {
+      type: TokenType.ERC20,
+      address: '0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d',
+      decimals: 18,
+      symbol: 'USDC',
+      name: 'USDC',
+      logoUri: '',
+    },
+    {
+      type: TokenType.ERC20,
+      address: '0x55d398326f99059fF775485246999027B3197955',
+      decimals: 18,
+      symbol: 'BSC-USD',
+      name: 'BSC-USD',
+      logoUri: '',
+    },
+    {
+      type: TokenType.ERC20,
+      address: '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56',
+      decimals: 18,
+      symbol: 'BUSD',
+      name: 'BUSD',
+      logoUri: '',
+    },
+    {
+      type: TokenType.ERC20,
+      address: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+      decimals: 18,
+      symbol: 'WBNB',
+      name: 'WBNB',
+      logoUri: '',
+    },
+  ]
+  const balances = new Map()
+  const promises = tokenlist.map((token) =>
+    getBalanceCall(address, token.address).then((balance) => balances.set(token.address, balance)),
   )
-  const [ETHBalance, Token1Balance] = await Promise.all([
-    web3.eth.getBalance(address),
-    erc20Instance.methods.balanceOf(address).call(),
-  ])
+  await Promise.all(promises)
+
   return {
     fiatTotal: '0',
-    items: [
-      {
-        tokenInfo: {
-          type: TokenType.NATIVE_TOKEN,
-          address: '0x0000000000000000000000000000000000000000',
-          decimals: 18,
-          symbol: 'BNB',
-          name: 'BNB',
-          logoUri: 'https://safe-transaction-assets.gnosis-safe.io/chains/56/currency_logo.png',
-        },
-        balance: new BigNumber(ETHBalance).toString(),
+    items: tokenlist.map((token) => {
+      return {
+        tokenInfo: token,
+        balance: new BigNumber(balances.get(token.address)).toString(),
         fiatBalance: '0.00000',
         fiatConversion: '0',
-      },
-      {
-        tokenInfo: {
-          type: TokenType.ERC20,
-          address: '0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d',
-          decimals: 18,
-          symbol: 'USDC',
-          name: 'USDC',
-          logoUri: '',
-        },
-        balance: new BigNumber(Token1Balance).toString(),
-        fiatBalance: '0.00000',
-        fiatConversion: '0',
-      },
-    ],
+      }
+    }),
   }
   // return getBalances(_getChainId(), address, selectedCurrency, {
   //   exclude_spam: excludeSpamTokens,

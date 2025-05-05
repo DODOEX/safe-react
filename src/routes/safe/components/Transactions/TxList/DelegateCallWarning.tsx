200099288605dc4cd6eeadec488d9aaee2684717
sa1i <sai@dodoex.io>
2025-05-05 20:46:08 +0800

    Add more warning info for delegate call

--- src/routes/safe/components/Transactions/TxList/DelegateCallWarning.tsx
+++ src/routes/safe/components/Transactions/TxList/DelegateCallWarning.tsx
@@ -4,7 +4,7 @@ import AlertTooltipWarning from './AlertTooltipWarning'
 
 const DelegateCallWarning = ({ showWarning = false }: { showWarning: boolean }): ReactElement => (
   <AlertTooltipWarning
-    tooltip="This transaction calls a smart contract that will be able to modify your Safe."
+    tooltip="We always warn of nested delegate calls: This transaction calls a smart contract that will be able to modify your Safe."
     message={showWarning ? 'Unexpected Delegate Call' : 'Delegate Call'}
     isWarning={showWarning}
   />
